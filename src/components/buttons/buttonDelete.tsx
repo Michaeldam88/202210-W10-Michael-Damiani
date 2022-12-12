6562c69e907a46a98f58b9839229eba4dba3bf81
Michaeldam88 <michaeldam88@gmail.com>
2022-12-12 18:57:33 +0100

    fixed the delete function

--- src/components/buttons/buttonDelete.tsx
+++ src/components/buttons/buttonDelete.tsx
@@ -1,13 +1,21 @@
+import { useRef } from 'react';
+
 export function ButtonDelete({
     removeElement,
 }: {
     removeElement: (id: string) => void;
 }) {
+    const ref = useRef<HTMLElement>(null);
 
-    const check = () =>{
-        console.log(removeElement("id_1"));
-    }
+    const deleteGentleman = () => {
+        if (ref && ref.current && ref.current.parentElement)
+            removeElement(ref.current.parentElement.id);
+    };
     return (
-        <i className="icon gentleman__icon gentleman__icon--delete fas fa-times" onClick={check}></i>
+        <i
+            ref={ref}
+            className="icon gentleman__icon gentleman__icon--delete fas fa-times"
+            onClick={deleteGentleman}
+        ></i>
     );
 }
